aefdd29416302f4018a95f445c4df710635eb0ec
손지원 <handbefore@sonjiwon-ui-MacBookAir.local>
2024-09-22 22:35:13 +0900

    feat:구조 완성

--- portfolio/src/components/Portfoilo.jsx
+++ portfolio/src/components/Portfoilo.jsx
@@ -4,20 +4,20 @@ const Portfolio = () => {
     return (
         <div>
         <div className='portfolioTitle'>Portfolio</div>
-        <div className='portfolioBoard'>
-            <div className='portfolioContent'>
-            <div>
-                &lt; mapping -
-                <span className='titleExpl'>할인 혜택부터 지출 관리까지, 지도로 한눈에!</span>
-            </div>
-            </div>
-            <div className='portfolioContentExpl'>
-            -&gt; 사용기술 : React, Node.js
-            </div>
-            <div className='portfolioContentExpl'>
-            -&gt; 역할 : 발표, 응애
-            </div>
-        </div>
+            <div className='portfolioBoard' onClick={() => window.location.href = 'https://www.naver.com/'}>
+                <div className='portfolioContent'>
+                    <div>
+                        &lt; mapping - 팀프로젝트 &nbsp;
+                        <span className='titleExpl'>할인 혜택부터 지출 관리까지, 지도로 한눈에!</span>
+                    </div>
+                </div>
+                <div className='portfolioContentExpl'>
+                    -&gt; 사용기술 : React, Node.js
+                    </div>
+                    <div className='portfolioContentExpl'>
+                    -&gt; 역할 : 발표, 응애
+                    </div>
+                </div>
         </div>
     );
 };
